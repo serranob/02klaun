225713a3349b1ce01090dfda94c7f842cc34a561
serranob <brenogserrano@hotmail.com>
2024-02-26 17:08:07 -0300

    locais alterados novamente, mas para pasta  correta dessa vez

---
+++ src/test/java/br/com/web2/kloun2/Kloun2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.web2.kloun2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kloun2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
